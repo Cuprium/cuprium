2e08cb01154b7a0c1a8f15aa1e697d6c56974cf5
Francis Fish <francis@francisfish.com>
2012-03-07 11:49:07 +0000

    Base Rails 3.2 app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cuprium::Application.config.secret_token = 'c7d99363200824b6bd42f072adc2476f651caaeeb25a637e0a6e3d5f8017ae833b615f20af4d17048fa0ee497cc34117a5239ae8d9deb20e35af47a27e222ce3'
