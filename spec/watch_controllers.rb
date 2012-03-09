d5dac886aec2b068b33c0931e0612f37066c82d0
Francis Fish <francis@francisfish.com>
2012-03-09 14:05:55 +0000

    get watchr to clear the screen

--- spec/watch_controllers.rb
+++ spec/watch_controllers.rb
@@ -1,2 +1,2 @@
-watch( 'spec/controllers/(.*)\.rb' )  {|md| system("bundle exec rspec #{md[0]}") }
-watch( 'app/controllers/(.*)\.rb' )  {|md| system("bundle exec rspec spec/controllers/#{md[1]}_spec.rb") }
+watch( 'spec/controllers/(.*)\.rb' )  {|md| system("clear; bundle exec rspec #{md[0]}") }
+watch( 'app/controllers/(.*)\.rb' )  {|md| system("clear; bundle exec rspec spec/controllers/#{md[1]}_spec.rb") }
