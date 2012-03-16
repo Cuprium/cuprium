6c68aebcdcd49edf576abb09a0bbef6aebc7ff09
Francis Fish <francis@francisfish.com>
2012-03-16 15:04:53 +0000

    run uniciorn, run, my beauty

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/usr/share/nginx/cuprium.railsbaby.com/current"
+pid "/usr/share/nginx/cuprium.railsbaby.com/shared/pids/unicorn.pid"
+stderr_path "/usr/share/nginx/cuprium.railsbaby.com/shared/log/unicorn.stderr"
+stdout_path "/usr/share/nginx/cuprium.railsbaby.com/shared/log/unicorn.stdout"
+
+listen "/tmp/cuprium-unicorn.todo.sock"
+worker_processes 2
+timeout 30
