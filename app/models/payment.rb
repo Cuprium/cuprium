13365c9ea94da4081540a71d3982977a226d3b40
Francis Fish <francis@francisfish.com>
2012-03-27 14:19:08 +0100

    removed unwanted code from payment and withdrawal ledger entry wrapper classes

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,14 +1,3 @@
 # This is a place holder class while we allow the objects to develop and grow
-require 'pstore'
-require 'account'
-require 'entry'
-require 'ledger'
-class Payment < Ledger
-  def self.entry
-    Entry.find 'payment'
-  end
-  def initialize(params={})
-    super
-    self.entry = self.class.entry
-  end
+class Payment < LedgerEntry
 end
--- app/models/withdrawal.rb
+++ app/models/withdrawal.rb
@@ -1,14 +1,3 @@
 # This is a place holder class while we allow the objects to develop and grow
-require 'pstore'
-require 'account'
-require 'entry'
-require 'ledger'
-class Withdrawal < Ledger
-  def self.entry
-    Entry.find 'withdrawal'
-  end
-  def initialize(params={})
-    super
-    self.entry = self.class.entry
-  end
+class Withdrawal < LedgerEntry
 end
