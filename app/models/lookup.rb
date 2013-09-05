70d4a6963a2b515bf14bcc43651f287fdfb41cae
Francis Fish <francis.fish@pharmarketeer.com>
2013-09-05 19:13:07 +0100

    Shook out some inconsistencies with the client's title

--- app/models/lookup.rb
+++ app/models/lookup.rb
@@ -1,4 +1,5 @@
 module Lookup
   # TODO - we will need to translate this eventually
   Title = ['Please select'  , 'Miss'  , 'Mr'  , 'Mrs' , 'Ms'  , 'Dr'  , 'Rev' , 'Other' ]
+  TitleList = Title.each_with_index.collect { |title,index| [title,index] }
 end
