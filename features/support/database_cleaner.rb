fce74657ab5e1e54cb39fab98e46937244bd7fb7
Francis Fish <francis.fish@pharmarketeer.com>
2013-09-05 10:50:03 +0100

    Moved creation of default ledger entry types into database cleaner Before block

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -8,7 +8,14 @@ rescue NameError
 end
 
 Before do
+  [ {name:'payment',direction:1},
+    {name:'withdrawal',direction:-1},
+    {name:'transfer_in',direction:1},
+    {name:'transfer_out',direction:-1},
+  ].each { |entry| Entry.find_by_name(entry[:name]) || Entry.create(entry) }
+
   DatabaseCleaner.start
+
 end
 
 After do |scenario|
--- features/support/local_env.rb
+++ features/support/local_env.rb
@@ -1,9 +1,2 @@
 World(FactoryGirl::Syntax::Methods)
 
-# TODO These are in the wrong place - need to be in a data migration
-[ {name:'payment',direction:1},
-  {name:'withdrawal',direction:-1},
-  {name:'transfer_in',direction:1},
-  {name:'transfer_out',direction:-1},
-].each { |entry| Entry.find_by_name(entry[:name]) || Entry.create(entry) }
-
